904cde9ab6abd9beb2a2ffd1a8086d538837681a
mnzooo <aldwn1124@gmail.com>
2022-09-21 20:41:33 +0900

    likelist db

---
+++ app/src/main/java/com/example/daily_fume/LikeRequest.java
@@ -0,0 +1,23 @@
+package com.example.daily_fume;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+public class LikeRequest extends StringRequest {
+    final static private String URL = "http://10.0.2.2/likelist.php";
+    private Map<String, String> parameters;
+
+    public LikeRequest(String listname, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("listname", listname);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+}
