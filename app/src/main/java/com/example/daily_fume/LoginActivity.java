66592b9a03f438309376cb3a8635f367eacb4144
EunHee <lehee0526@gmail.com>
2022-10-18 21:30:40 +0900

    Resign & user update

--- app/src/main/java/com/example/daily_fume/LoginActivity.java
+++ app/src/main/java/com/example/daily_fume/LoginActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -22,7 +26,31 @@ import com.kakao.auth.Session;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.Account;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private static String TAG = "phplogin";
+
+    private static final String TAG_JSON = "user";
+    private static final String TAG_EMAIL = "uemail";
+    private static final String TAG_PASS = "upassword";
+    private static final String TAG_NAME = "uname";
+    private static final String TAG_BIRTH = "ubirth";
+
+    ArrayList<HashMap<String, String>> mArrayList;
+    private EditText mEditTextID, mEditTextPass;
+    Button loginBtn;
+
+    String loginSort;
+    String result;
 
     ImageView backBtn, joinBtn;
     TextView title_change;
@@ -31,6 +59,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEditTextID = (EditText) findViewById(R.id.idTextBox);
+        mEditTextPass = (EditText) findViewById(R.id.pwTextBox);
+
+        mEditTextPass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.topBar);
         setSupportActionBar(toolbar);
@@ -56,6 +89,17 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mArrayList.clear();
+                GetData task = new GetData();
+                task.execute(mEditTextID.getText().toString(), mEditTextPass.getText().toString());
+            }
+        });
+        mArrayList = new ArrayList<>();
+
         //카카오톡 로그인 구현
 
         ImageButton kakaoLogin = (ImageButton) findViewById(R.id.kakaoLogin);
@@ -64,12 +108,94 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(LoginActivity.this)) {
                     login();
+                    redirectSignUpActivity();
                 } else {
                     accountLogin();
                 }
             }
         });
     }
+
+    private class GetData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(LoginActivity.this, "Please Wait", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            Log.d(TAG, "response" + result);
+
+
+            if(result == null) {
+                Toast.makeText(getApplicationContext(), "아이디 또는 비밀번호가 틀립니다.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String uemail = (String)params[0];
+            String upassword = (String)params[1];
+
+            String serverURL = "http://43.201.60.239/login.php";
+            String postParameters = "uemail=" + uemail + "&upassword=" + upassword;
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+                return sb.toString();
+
+            } catch (Exception e) {
+                Log.d(TAG, "InsertData: Error ", e);
+                return new String("Error: " + e.getMessage());
+            }
+        }
+    }
+
+
 
     public void login() {
         String TAG = "login()";
@@ -107,7 +233,7 @@ public class LoginActivity extends AppCompatActivity {
                 Log.i(TAG, user.toString());
                 {
                     Log.i(TAG, "사용자 정보 요청 성공" +
-                            "\n회원번호: " + user.getId() +
+                            "\n닉네임: " + user.getId() +
                             "\n이메일: " + user.getKakaoAccount().getEmail());
                 }
                 Account user1 = user.getKakaoAccount();
@@ -117,6 +243,11 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    protected void redirectSignUpActivity() {
+        final Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
+        finish();
+    }
 
 //        viewInit();
 //
@@ -136,30 +267,6 @@ public class LoginActivity extends AppCompatActivity {
 
 
     //}
-
-
-    void showLoginBack() {
-        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(LoginActivity.this)
-                .setTitle("알림")
-                .setMessage("앱을 종료하시겠습니까?")
-                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        int end = android.os.Process.myPid();
-                        android.os.Process.killProcess(end);
-                        finish();
-                    }
-                })
-                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int which) {
-                        dialogInterface.cancel();
-                    }
-                });
-        AlertDialog msgDlg = msgBuilder.create();
-        msgDlg.show();
-    }
-}
 
 
 //    //카카오톡 로그인 구현
@@ -188,3 +295,26 @@ public class LoginActivity extends AppCompatActivity {
 //        Session.getCurrentSession().removeCallback((ISessionCallback) KakaoCallBack);
 //    }
 //}
+
+    void showLoginBack() {
+        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(LoginActivity.this)
+                .setTitle("알림")
+                .setMessage("앱을 종료하시겠습니까?")
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        int end = android.os.Process.myPid();
+                        android.os.Process.killProcess(end);
+                        finish();
+                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog msgDlg = msgBuilder.create();
+        msgDlg.show();
+    }
+}
---
+++ app/src/main/java/com/example/daily_fume/ResignActivity.java
@@ -0,0 +1,124 @@
+package com.example.daily_fume;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.kakao.sdk.user.model.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ResignActivity extends AppCompatActivity {
+
+    Button button3;
+    String listname;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resign);
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setClickable(true);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final EditText user_email = new EditText(ResignActivity.this);
+
+                // 1) 이메일 인증 창
+                AlertDialog.Builder msgBuilder = new AlertDialog.Builder(ResignActivity.this)
+                        .setTitle("\"DailyFume\"을 탈퇴하시려면 이메일 인증이 필요합니다.")
+                        .setMessage("이메일을 입력해주세요.")
+                        .setView(user_email)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // 입력된 이메일과 데이터베이스 대조
+                                listname = user_email.getText().toString();
+                                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                                    @Override
+                                    public void onResponse(String response) {
+                                        System.out.print(response);
+                                        try {
+                                            JSONObject jsonObject = new JSONObject(response);
+                                            boolean success = jsonObject.getBoolean("success");
+
+                                            if (success) {
+                                                // 2) 이메일 일치 > 최종 확인 창 출력
+                                                
+                                                //TitleValues.add(listname); -- 안 쓰는 코드
+                                                //pickBoxLoading();
+
+                                                Toast.makeText(getApplicationContext(), "이메일이 일치합니다.", Toast.LENGTH_SHORT).show();
+
+                                                AlertDialog.Builder msg_final = new AlertDialog.Builder(ResignActivity.this)
+                                                        .setTitle("이 동작은 취소할 수 없습니다.")
+                                                        .setMessage("정말로 \"DailyFume\"을 탈퇴하시겠습니까?")
+                                                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                                            @Override
+                                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                                // 3) 최종 확인 창에서 확인 > 데이터베이스에서 삭제 후 로그인 페이지로 이동
+                                                                Toast.makeText(getApplicationContext(), "탈퇴가 정상적으로 처리되었습니다.", Toast.LENGTH_SHORT).show();
+                                                                //스택을 비우고 새로운 액티비티 띄우기
+                                                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                                                startActivity(intent);
+                                                            }
+                                                        })
+                                                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                                            @Override
+                                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                                // 3) 최종 확인 창에서 취소 > 마이페이지 창으로 돌아가기
+                                                                Intent intent = new Intent(getApplicationContext(), UserPrivacyActivity.class);
+                                                                startActivity(intent);
+                                                            }
+                                                        });
+                                                AlertDialog msgDlg_fin = msg_final.create();
+                                                msgDlg_fin.show();
+
+                                            } else {
+                                                // 2) 이메일 불일치 > 토스트 메시지
+                                                Toast.makeText(getApplicationContext(), "이메일이 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                                            }
+                                        } catch (JSONException e) {
+                                            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                };
+
+                                DeleteRequest delete_user = new DeleteRequest(listname, responseListener);
+                                RequestQueue queue = Volley.newRequestQueue(ResignActivity.this);
+                                queue.add(delete_user);
+
+                            }
+                        })
+                        // 1) 이메일 인증 취소
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogInterface, int whichButton) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                AlertDialog msgDlg = msgBuilder.create();
+                msgDlg.show();
+
+            }
+        });
+
+    }
+
+
+}
